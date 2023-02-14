0c779f616a2b13626673f888ef023365cf06a7dd
FPAlejandro <112148961+FPAlejandro@users.noreply.github.com>
2023-02-14 09:43:50 -0600

    Agregando codigo
    Agregando librería y código de condición

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 print('Hola mi nombre es: Juan Alejandro Medina Marroquín')
+
+import os
+
+mi_ubicacion = os.getcwd()
+if os.path.exists("modulos"):
+    print("La carpeta ya existe")
+else:
+    os.mkdir(mi_ubicacion + "\\modulos")
+    archivo = open('./modulos/prueba.txt','w')
+    archivo.write('Hola mundo')
+    archivo.close()
