1d1d70d7fce27e6e43ad20a8a5808ad3a33eb3f7
FPAlejandro <medinale963@outlook.es>
2023-02-14 10:20:27 -0600

    Archivo py
    Agregando el codigo que se nos proporciono, este agrego una nueva carpeta y archivo .txt

--- mayn.py
+++ mayn.py
@@ -1,3 +1,13 @@
 print('Hola mi nombre es: Juan Alejandro Medina Marroquín')
 print('Mi carné es: 1597523')
 
+import os
+
+mi_ubicacion = os.getcwd()
+if os.path.exists("modulos"):
+    print("La carpeta ya existe")
+else:
+    os.mkdir(mi_ubicacion + "\\modulos")
+    archivo = open('./modulos/prueba.txt', 'w')
+    archivo.write('Hola mundo')
+    archivo.close()
